ad68ad5085ba2112dd038043727394ce1b5e750c
Allison Usher <allyjay317@gmail.com>
2020-10-14 16:20:19 -0700

    removed user seed, redundent truncate in role seed

--- data/seeds/01-roles.js
+++ data/seeds/01-roles.js
@@ -1,12 +1,8 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('roles').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('roles').insert([
-        { name: 'user' },
-        { name: 'owner' },
-      ]);
-    });
+  return knex('roles').insert([
+    { name: 'user' },
+    { name: 'owner' },
+  ]);
 };
