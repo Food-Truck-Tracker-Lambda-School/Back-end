8dc02ee5ebc753642b405a677b420c6d4598938b
Allison Usher <allyjay317@gmail.com>
2020-10-19 10:39:51 -0700

    test

--- api/server-error-middleware.js
+++ api/server-error-middleware.js
@@ -1,16 +1,16 @@
 const { json } = require('express')
 const db = require('../data/connection')
 
-module.exports = function (err, req, res, next) {
+module.exports = async function (err, req, res, next) {
   if (err) {
     try {
       console.log(err)
-      db.insert({
+      await db.insert({
         errorDate: Date.now(),
         error: JSON.stringify(err)
       })
     } catch (error) {
-
+      console.log(error)
     }
     res.status(500).json({ message: 'a server error has occured' })
   }
